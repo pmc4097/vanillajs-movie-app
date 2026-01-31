64b7cf7dd898eac615fcd7161756fff8d42e9a72
charles <mcpark4097@gmail.com>
2026-01-31 16:52:25 +0900

    수정

--- src/store/movie.js
+++ src/store/movie.js
@@ -20,9 +20,13 @@ export const searchMovies = async (page) => {
   }
 
   try {
-    const res = await fetch(
-      `https://www.omdbapi.com/?apikey=7035c60c&s&s=${store.state.searchText}&page=${page}`,
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page,
+      }),
+    });
     const { Search, totalResults, Response, Error } = await res.json();
     if (Response === "True") {
       store.state.movies = [...store.state.movies, ...Search];
@@ -40,9 +44,12 @@ export const searchMovies = async (page) => {
 
 export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(
-      `https://www.omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`,
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({
+        id,
+      }),
+    });
     store.state.movie = await res.json();
   } catch (error) {
     console.error("getMovieDetails error", error);
