280ce5c88a57e618f1437a9210f6ce7505c06794
charles <mcpark4097@gmail.com>
2026-01-31 17:01:30 +0900

    수정

--- api/movie.js
+++ api/movie.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const { API_KEY } = process.env;
+const { APIKEY } = process.env;
 
 export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body);
 
   const url = id
-    ? `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}&plot=full`
-    : `https://www.omdbapi.com/?apikey=${API_KEY}&s=${title}&page=${page}`;
+    ? `https://www.omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full`
+    : `https://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`;
 
   const res = await fetch(url);
   const json = await res.json();
